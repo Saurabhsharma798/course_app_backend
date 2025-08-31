2a17e76b48a2199f66f6976c7b1267ed4b3bcc3f
Saurabhsharma798 <k.saurabhsharma798@gmail.com>
2025-08-31 18:19:10 +0530

    admin signup route done

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,18 @@
 const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin")
+const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router=Router()
 
-router.post('/signup',adminMiddleware,(req,res)=>{
-
+router.post('/signup',async (req,res)=>{
+    const username = req.headers.username
+    const password = req.headers.password
+    
+    const userCreated = await Admin.create({username,password})
+    if (userCreated){
+        res.status(200).json({msg:"Admin created successfully"})
+    }else{
+        res.status(403).json({msg:"error while creating Admin"})
+    }
 });
 
 router.post('/courses',adminMiddleware,(req,res)=>{
