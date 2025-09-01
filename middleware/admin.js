c2871b21cf38fd6fda6afc1e91105cf9804bbeb3
Saurabhsharma798 <k.saurabhsharma798@gmail.com>
2025-09-01 13:22:44 +0530

    .gitignore added

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,20 +2,24 @@ const { Admin } = require("../db/index")
 
 
 async function adminMiddleware(req,res,next){
-    const username = req.headers['username']
-    const password =  req.headers['password']
-    const value = await Admin.findOne({
-        username:username,
-        password:password
-    })
-    console.log(value)
-    if (value){
-        return next();
-    }else{
-        return res.status(403).json({
-            msg:"user doesn't exist"
-        })
-    }
+    // const username = req.headers['username']
+    // const password =  req.headers['password']
+    // const value = await Admin.findOne({
+    //     username:username,
+    //     password:password
+    // })
+    // console.log(value)
+    // if (value){
+    //     return next();
+    // }else{
+    //     return res.status(403).json({
+    //         msg:"user doesn't exist"
+    //     })
+    // }
+
+    const token=req.headers.authorization
+    const words=token.split(" ")
+    const jwtToken=words[1]
 
 }
 
